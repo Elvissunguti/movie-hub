830dd96238ad29396ca2d45bab9be62dec4303f0
Elvis Sunguti <elviskendrick99@gmail.com>
2023-04-13 06:32:56 +0300

    added function to about

--- src/Components/About/Download.jsx
+++ src/Components/About/Download.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import download from "../Assets/download.jpg";
 
 const Download = () => {
     return (
-        <div>
-            Download to watch later
-        </div>
+        <section className="bg-[#000000] border-b-8 border-gray-800 border-solid">
+            <div className="flex lg:flex-row max-w-4xl max-w-6xl mx-auto">
+                <div className="lg:w-1/2">
+                    <img src={download} alt="" className="" />
+                </div>
+                <div className="text-white flex flex-col items-center justify-center lg:w-1/2">
+                    <h2 className="font-bold text-5xl">Download your shows to watch offline.</h2>
+                    <p className="font-semibold text-xl">Save your favorites easily and always have something to watch.</p>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Download;
