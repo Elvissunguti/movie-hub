a0eb07c40838f2d4caae3ada25ab33b40a58680b
Elvis Sunguti <elviskendrick99@gmail.com>
2023-04-13 07:26:26 +0300

    Added features to about

--- src/Components/About/CreateProfile.jsx
+++ src/Components/About/CreateProfile.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import createProfile from "../Assets/createProfile.png";
 
 const CreateProfile = () => {
     return(
-        <div>
-            How to create a profile
-        </div>
+        <section className="bg-[#000000] border-b-8 border-gray-800 border-solid">
+            <div className="flex flex-col-reverse lg:flex-row max-w-4xl xl:max-w-6xl mx-auto">
+                <div className="lg:w-1/2 ">
+                    <img src={createProfile} alt="" />
+                </div>
+                <div className="text-white flex flex-col items-center justify-center lg:w-1/2">
+                    <h2 className="font-bold text-5xl">Create profiles for kids.</h2>
+                    <p className="font-semibold text-xl">
+                        Send kids on adventures with their favorite characters in a space made just for them—free with your membership.
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
 export default CreateProfile;
