e9acb293d8a804a2dcbd9556fbe1d469f1256ff9
Elvis Sunguti <elviskendrick99@gmail.com>
2023-04-13 07:02:43 +0300

    added features to about

--- src/Components/About/Download.jsx
+++ src/Components/About/Download.jsx
@@ -4,7 +4,7 @@ import download from "../Assets/download.jpg";
 const Download = () => {
     return (
         <section className="bg-[#000000] border-b-8 border-gray-800 border-solid">
-            <div className="flex lg:flex-row max-w-4xl max-w-6xl mx-auto">
+            <div className="flex flex-col-reverse lg:flex-row max-w-4xl max-w-6xl mx-auto">
                 <div className="lg:w-1/2">
                     <img src={download} alt="" className="" />
                 </div>
--- src/Components/About/Watch.jsx
+++ src/Components/About/Watch.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import watch from "../Assets/watch.png";
 
 const Watch = () => {
     return(
-        <div>
-            Watch everywhere
+      <section className="bg-[#000000] border-b-8 border-gray-800 border-solid ">
+        <div className="flex flex-col lg:flex-row max-w-4xl max-w-6xl mx-auto">
+            <div className="lg:w-1/2 text-white flex flex-col items-center justify-center">
+                <h2 className="text-5xl font-bold">Watch everywhere.</h2>
+                <p className="font-semibold text-xl">
+                    Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.
+                </p>
+            </div>
+            <div className="lg:w-1/2">
+                <img src={watch} alt="" />
+            </div>
         </div>
+      </section>
     )
 }
 export default Watch;
