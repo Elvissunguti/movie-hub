9c76697794a10074713a7ac2a211a8d1819ac365
Elvis Sunguti <elviskendrick99@gmail.com>
2023-04-13 11:25:47 +0300

    added features to About

--- src/Components/About/Questions.jsx
+++ src/Components/About/Questions.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from "./AboutData";
 
 const Questions = () => {
+
+    const [openIndex, setOpenIndex] = useState(-1);
+
+    const handlePanelClick = (index) => {
+      setOpenIndex((prevIndex) => (prevIndex === index ? -1 : index));
+    };
+
+
     return(
-        <div>
-            Question asked
+        <section className="bg-[#000000]">
+            <div className="max-w-4xl xl:max-w-6xl mx-auto ">
+                <div className="text-white  ">
+                    <h2 className="text-2xl font-bold">
+                        Frequently Asked Questions
+                    </h2>
+                    <div>
+                       {data.map((panel, index) => (
+          <div className="panel" key={index}>
+          <div className={`panel-header ${openIndex === index ? 'open' : ''}`} onClick={() => handlePanelClick(index)}>
+            {panel.question}
+          </div>
+          <div className={`panel-body ${openIndex === index ? 'open' : ''}`}>
+            {panel.answer}
+          </div>
         </div>
+      ))}
+
+                        </div>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Questions;
