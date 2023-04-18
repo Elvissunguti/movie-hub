25f17f018d287cb3e53567972e1e7b0f196e60b8
Elvis Sunguti <elviskendrick99@gmail.com>
2023-04-18 16:12:07 +0300

    added functionality

--- src/Components/BrowsePage/BrowsePage.jsx
+++ src/Components/BrowsePage/BrowsePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import Row from "./Row";
+import requests from "../../request";
 
 const BrowsePage = () => {
     return(
@@ -8,7 +10,19 @@ const BrowsePage = () => {
                 <NavBar />
             </div>
             <div>
-                Browse page
+            <Row
+        title={"NETFLIX ORIGINALS"}
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title={"Trending Now"} fetchUrl={requests.fetchTrending} />
+      <Row title={"History Movies"} fetchUrl={requests.fetchHistoryMovies} />
+      <Row
+        title={"Animation Movies"}
+        fetchUrl={requests.fetchAnimationMovies}
+      />
+      <Row title={"Fantasy Movies"} fetchUrl={requests.fetchFantasyMovies} />
+      <Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies} />
             </div>
 
         </section>
