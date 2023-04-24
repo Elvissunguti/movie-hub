14018d5014835396df1f892342dab1e90b80922b
Elvis Sunguti <elviskendrick99@gmail.com>
2023-04-24 07:37:17 +0300

    added functionality to sign in

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,32 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+
+    const [ email, setEmail] = useState('');
+    const [ password, setPassword] = useState('');
+    const [ isFocused, setIsFocused] = useState(false);
+
+    const handleInputChangeEmail = (e) => {
+        setEmail(e.target.value)
+    };
+
+    const handleInputChangePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleInputFocus = () => {
+        setIsFocused(true);
+    };
+
+    const handleInputBlur = () => {
+        setIsFocused(false)
+    }
     return(
         <section className="">
-            <div className="w-full h-screen relative">
+            <div className="w-full h-screen mx-auto relative">
                 <div className="bg-custom absolute inset-0">
-                    <form className="relative z-10 text-white">
-                        <div className="flex flex-col gap-5 items-center justify-center">
-                            <h2>Sign In</h2>
-                            <label>
+                    <form className="absolute w-full h-screen flex items-center justify-center flex-col text-white">
+                        <div className="flex flex-col space-y-5 items-center justify-center">
+                            <h2 className="text-3xl font-bold">Sign In</h2>
+                            <div>
+                            <label htmlFor="email"
+                                    className={`absolute transition-all duration-200 pointer-events-none ${
+                                        isFocused || email ? 'text-sm text-gray-500 top-0 ' : 'text-base text-gray-400'
+                                      }`}
+                            >
+                                Email Or phone number
+                                </label>
+
                             <input
                             type="email"
                             name="email"
+                            id="email"
+                            value={email}
+                            autoComplete="email"
                             required
-                            placeholder="Email or phone number"
+                            placeholder=" "
                             className=""
+                            onChange={handleInputChangeEmail}
+                            onFocus={handleInputFocus}
+                            onBlur={handleInputBlur}
                             />
-                            </label>
-                            <label>
+                            </div>
+
+
+
+                            <div>
+                            <label htmlFor="password"
+                             className={`absolute transition-all duration-200 pointer-events-none ${
+                                isFocused || password ? 'text-sm text-gray-500 top-3' : 'text-base text-gray-400'
+                             }`}
+                             >
+                                Password
+                                </label>
                             <input
                             type="password"
                             name="password"
+                            id="email"
                             required
-                            placeholder="Password"
-                            className=""
+                            value={password}
+                            placeholder=" "
+                            className="text-black"
+                            onChange={handleInputChangePassword}
+                            onFocus={handleInputFocus}
+                            onBlur={handleInputBlur}
                             />
-                            </label>
+                            </div>
+                            
                             
                         </div>
                         <div>
